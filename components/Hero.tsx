b7572f66fec1b588bbada8ee3c84a3eff87e6faa
jagxman <jagjitmangar177@gmail.com>
2023-08-05 01:36:03 -0400

    Layout implemented

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,6 +27,8 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
             <Image src="/hero.png" alt="hero" fill className="object-contain"></Image>
+
+            <div className="hero__image-overlay"></div>
         </div>
       </div>
     </div>
