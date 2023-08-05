4e72356478fa4c476529186e402e87916de5dd0c
jagxman <jagjitmangar177@gmail.com>
2023-08-05 02:36:27 -0400

    Main showcase fix with image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,8 +28,8 @@ const Hero = () => {
         <div className="hero__image">
             <Image src="/hero.png" alt="hero" fill className="object-contain"></Image>
 
-            <div className="hero__image-overlay"></div>
         </div>
+        <div className="hero__image-overlay"/>
       </div>
     </div>
   );
