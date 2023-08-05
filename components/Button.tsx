fbb555d00602ecd80e172f63370b1edc7abcef92
jagxman <jagjitmangar177@gmail.com>
2023-08-05 01:51:01 -0400

    Navbar Completion

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,11 +2,11 @@
 import { ButtonProps } from "@/types"
 import Image from "next/image"
 
-const Button = ({title, containerStyles, handleClick}: ButtonProps) => {
+const Button = ({title, btnType, containerStyles, handleClick}: ButtonProps) => {
 
   return (
     
-    <button disabled={false} type={'button'} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
+    <button disabled={false} type={btnType} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
 
         <span className={`flex-1`}>{title}</span>
 
--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,7 @@ import { MouseEventHandler } from "react";
 
 export interface ButtonProps {
     title: string;
+    btnType?:'submit' | 'reset' | 'button' | undefined;
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>
 }
